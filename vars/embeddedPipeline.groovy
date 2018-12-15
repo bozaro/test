0805fe5640db4437b713e978fdf4fe1c20e2c501
Artem V. Navrotskiy <bozaro@yandex.ru>
2018-12-15 16:40:45 +0300

    fixup! fixup! fixup! Embedded Jenkins pipeline example

--- vars/embeddedPipeline.groovy
+++ vars/embeddedPipeline.groovy
@@ -1,10 +1,16 @@
 def call(Map pipelineParams) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
     pipeline {
         agent any
         stages {
             stage('build') {
                 steps {
+                    echo pipelineParams.message
                     sh """
 env | sort
 """
